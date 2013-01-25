958e52be985d606ab60cf842d3e215c44dab75b7
martin <martin@cbf26c83-b0a1-4be6-a259-b40729d9ac68>
2013-01-25 23:26:15 +0000

    Make sure to get this file PAUL!

--- Terptube-Mockup-Paul/js/index/martinFunctions.js
+++ Terptube-Mockup-Paul/js/index/martinFunctions.js
@@ -42,7 +42,7 @@ function popUpRecorder(element, feature, fileName, type)
 //			$("#"+element).load("recordOrPreview/index.php?feature=record");
 		},
 		create: function(event, ui) {
-			$(event.target).parent().css('position', 'relative');
+			$(event.target).parent().css('position', 'relative'); //Dumb comment at this line!
 		},
 		position: { my: "center top", at: "center top", of: $("body") },
 		show: "blind",
